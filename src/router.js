c5922dc937e94a21ce3c8042bbe3bf5e8fd50d2a
FIRST_NAME LAST_NAME <kv3delacruz@gmail.com>
2021-08-10 16:49:35 -0600

    Velocity animations added

--- src/router.js
+++ src/router.js
@@ -29,7 +29,16 @@ export default new Router({
          // this generates a separate chunk (about.[hash].js) for this route
          // which is lazy-loaded when the route is visited.
          component: () =>
-            import(/* webpackChunkName: "about" */ './views/List.vue')
+            import(/* webpackChunkName: "list" */ './views/List.vue')
+      },
+      {
+         path: '/drawer',
+         name: 'drawer',
+         // route level code-splitting
+         // this generates a separate chunk (about.[hash].js) for this route
+         // which is lazy-loaded when the route is visited.
+         component: () =>
+            import(/* webpackChunkName: "drawer" */ './views/Drawer.vue')
       }
    ]
 })
